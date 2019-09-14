f09fd8a574e64126d5b70da8c5dc43c9bf9ed5b5
raspi <raspi@raspi>
2019-09-14 08:29:48 +0000

    product

--- python.py
+++ python.py
@@ -17,14 +17,17 @@ def wait_signal(key):
     server_ip = "192.168.8.102"
     port = 50008
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    s.settimeout(30.0)
+    #s.settimeout(30.0)
     s.bind((server_ip, port))
     try:
         data, addr = s.recvfrom(1024)
         print("data: {}, addr: {}".format(data, addr))
+        if data == key:
+            return True
     except socket.timeout:
         print("timeout")
-
+    return False
+        
 # init servo
 gp_list = [4, 17]
 servos = []
@@ -35,23 +38,31 @@ for n in gp_list:
     servo.start(0)
     servos.append(servo)
 
-wait_signal("ready")
-
-# wait signal
-servos[0].ChangeDutyCycle(6)
-time.sleep(1)
-#servos[1].ChangeDutyCycle(4.7)    # batter 
-servos[1].ChangeDutyCycle(12)    # pitcher
-#time.sleep(5)
-
-wait_signal("swing")
+while True:
+    ret = wait_signal(b"ready")
+    print(ret)
     
-# get signal
-servos[0].ChangeDutyCycle(1)    # about 80 degree
-time.sleep(2)
-#servos[1].ChangeDutyCycle(9.3)    # batter
-servos[1].ChangeDutyCycle(1)   # pitcher
-time.sleep(3)
+    if not ret:
+        continue
+
+    # wait signal
+    servos[0].ChangeDutyCycle(6)
+    time.sleep(1)
+    #servos[1].ChangeDutyCycle(4.7)    # batter 
+    servos[1].ChangeDutyCycle(12)    # pitcher
+    #time.sleep(5)
+
+    ret = wait_signal(b"swing")
+    print(ret)
+    if not ret:
+        continue
+        
+    # get signal
+    servos[0].ChangeDutyCycle(1)    # about 80 degree
+    time.sleep(2)
+    #servos[1].ChangeDutyCycle(9.3)    # batter
+    servos[1].ChangeDutyCycle(1)   # pitcher
+    time.sleep(3)
 
 # term
 for servo in servos:
