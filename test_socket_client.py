8fd3a6cf7b86d303239237621588fa370590aa1b
aiueo <aiceford99maim@gmail.com>
2019-09-14 14:19:31 +0900

    add timeout

--- test_socket_client.py
+++ test_socket_client.py
@@ -1,6 +1,6 @@
 import socket
 
-server_ip = "000.000.00.000"
+server_ip = "000.000.000.0"
 port = 50007
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.sendto(b'Hello UDP', (server_ip, port))
--- test_socket_server.py
+++ test_socket_server.py
@@ -3,7 +3,12 @@ import socket
 server_ip = "000.000.00.000"
 port = 50007
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.settimeout(3.0)
 s.bind((server_ip, port))
 while True:
-    data, addr = s.recvfrom(1024)
-    print("data: {}, addr: {}".format(data, addr))
+    try:
+        data, addr = s.recvfrom(1024)
+    except socket.timeout:
+        print("timeout")
+    else:
+        print("data: {}, addr: {}".format(data, addr))
